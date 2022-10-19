a93284b6622058af09e98d97d2f9670e785fb3cc
alemtideg <arsematebkgn@gmail.com>
2022-10-19 09:22:03 -0700

    main.h

--- main.h
+++ main.h
@@ -1,17 +1,12 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
 #include <stdarg.h>
-
 #include <stdio.h>
-
 #include <unistd.h>
 
 
 
 #define UNUSED(x) (void)(x)
-
 #define BUFF_SIZE 1024
 
 
@@ -19,13 +14,9 @@
 /* FLAGS */
 
 #define F_MINUS 1
-
 #define F_PLUS 2
-
 #define F_ZERO 4
-
 #define F_HASH 8
-
 #define F_SPACE 16
 
 
@@ -33,7 +24,6 @@
 /* SIZES */
 
 #define S_LONG 2
-
 #define S_SHORT 1
 
 
